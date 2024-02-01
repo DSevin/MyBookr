0d6f2813bbab1350b7599a9014745a7e9eb572ef
Kevin Gopito <kevingopito@gmail.com>
2024-02-01 15:40:41 +0200

    tweeks for vercel app

--- bookr/settings.py
+++ bookr/settings.py
@@ -31,7 +31,7 @@ class Dev(Configuration):
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = values.BooleanValue(True)
 
-    ALLOWED_HOSTS = values.ListValue([])
+    ALLOWED_HOSTS = values.ListValue(['.vercel.app'])
 
     # Application definition
 
